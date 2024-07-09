5722ac564e207f982edd736a532a31aae696d224
LESTA\a_gavrilushkin <a_gavrilushkin@lesta.group>
2024-07-09 12:31:56 +0300

    linked cancellation token update

---
+++ Runtime/CancellationToken/LinkedCancellationTokenSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Laphed.ScenariosUI
+{
+    public class LinkedCancellationTokenSource : IDisposable
+    {
+        public CancellationToken Token => internalCancellationTokenSource.Token;
+        public bool IsCancellationRequested => internalCancellationTokenSource.IsCancellationRequested;
+
+        private readonly CancellationTokenSource internalCancellationTokenSource;
+        private readonly CancellationTokenRegistration parentTokenRegistration;
+
+        public LinkedCancellationTokenSource(CancellationToken parentToken)
+        {
+            internalCancellationTokenSource = new CancellationTokenSource();
+
+            parentTokenRegistration = parentToken.Register(Cancel);
+        }
+
+        public void Cancel()
+        {
+            internalCancellationTokenSource.Cancel();
+        }
+
+        public void Dispose()
+        {
+            parentTokenRegistration.Dispose();
+            internalCancellationTokenSource.Dispose();
+        }
+    }
+}
